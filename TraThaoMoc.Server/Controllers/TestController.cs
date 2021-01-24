1defcc6d1bd2de41f910b17c27415cc69dfc0b66
Tung <tungngocitus@gmail.com>
2021-01-24 17:45:22 +0700

    Init app

---
+++ TraThaoMoc.Server/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace TraThaoMoc.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController{
+        [HttpGet("products")]
+        public List<Product> GetProduct([FromQuery]int numberOfProduct, [FromQuery] string searchBy)
+        {
+            var products = new List<Product>
+            { 
+                new Product { Id = 1, Name = "Bot Gung", Description = "San pham chong viem"},
+                new Product { Id = 1, Name = "Dau gio", Description = "Chua tinh dau bac ha"},
+                new Product { Id = 1, Name = "La xong", Description = "Khong hoa chat"}
+
+            };
+
+            if(!string.IsNullOrEmpty(searchBy)){
+                products = products.Where(s => s.Name.ToLower().Contains(searchBy)).ToList();
+            }
+            
+            return products.Take(numberOfProduct).ToList();
+        }
+    }
+}
