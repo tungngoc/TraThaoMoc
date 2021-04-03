f5db9f2a7718a4868186e63730384864dd19a832
DESKTOP-L4FTB5J\Win 10 <nnbduy239@gmail.com>
2021-04-03 17:55:28 +0700

    commit with error "Failed to load resource: net::ERR_EMPTY_RESPONSE" cant fix

--- TraThaoMoc.Client/thaomoc-client/src/App.js
+++ TraThaoMoc.Client/thaomoc-client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Component } from "react";
+import axios from "axios";
+class App extends Component {
+  state = {
+    message: "",
+  };
+  componentDidMount() {
+    axios
+      .get("http://localhost:5001/home/ping")
+      .then((reponse) => this.setState({ message: reponse.data.message }))
+      .catch(error=>{console.log(error)});
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.message}</h1>
+        <h2>complete</h2>
+      </div> 
+    );
+  }
 }
-
 export default App;
+// class App extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state ={
+//       items :[],
+//       isloader: false,
+//     }
+//   }
+//   componentDidMount(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res =>res.json())
+//     .then(json =>{
+//       this.setState({
+//         isloader: true,
+//         items: json,
+//       })
+//     });
+//   }
+//   render(){
+//     var {items} = this.state;
+//     return(
+//       <div className="App">
+//        <ul>
+//          { items.map(item=>(
+//             <li key={item.id}>
+//               Name : {item.name}| Email:{item.email} 
+//             </li>
+//          ))};
+//        </ul>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
--- TraThaoMoc.Server/Controllers/HomeController.cs
+++ TraThaoMoc.Server/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-
 namespace TraThaoMoc.Server.Controllers
 {
     [ApiController]
